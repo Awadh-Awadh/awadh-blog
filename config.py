abf7058ee03e57f3dc66385ff787318d40626856
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-22 00:44:23 +0300

    app setup

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+
+class Config():
+  pass
+
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+
+
+config_options = {
+  'development':DevConfig,
+  'production':ProdConfig
+}
