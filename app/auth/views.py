e5970c1adfbf61d629ade2ab496c79c12eca6d9b
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-22 22:39:47 +0300

    add logout route and templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,10 @@
 from flask import render_template, redirect,url_for
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 from . import auth
 from .forms import LoginForm, RegisterForm
 from ..models import User,Posts
 from .. import db,bcrypt
-from flask_login import login_user,current_user
+from flask_login import login_user,current_user, logout_user,login_required
 
 
 @auth.route('/register')
@@ -34,3 +34,8 @@ def register():
         return redirect(url_for('main.index'))
 
     return render_template('auth/login.html', form = form)
+
+  @auth.route('/logout')
+  @login_required
+  def logout():
+      logout_user()
