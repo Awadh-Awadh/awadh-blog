1e2fbb0eb296bd4e5fc843f63486eeda520b78b0
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-22 20:03:47 +0300

    add post model and create a relationship

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from enum import unique
+from sqlalchemy.orm import backref
 
 from wtforms.fields.simple import PasswordField
 from wtforms.validators import ValidationError
@@ -6,11 +7,21 @@ from . import db
 #from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class user(db.Model):
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key = True)
     username = db.Column(db.string(20), nullable = False)
     email = db.Column(db.String(), nullable = False)
-    password_hash = db.Column(db.String(), unique = True, nullable= False)
+    posts = db.relationship('Posts', backref = 'author', lazy = 'dynamic')
+
+
+class Posts (db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer(), primary_key = True)
+    title = db.Column(db.string(20), nullable = False)
+    content = db.Column(db.String(255))
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
 
 
 
