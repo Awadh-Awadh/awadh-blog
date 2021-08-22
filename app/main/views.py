852932018539930218a171b4baa69643af6b7394
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-22 10:36:00 +0300

    get data from api

---
+++ app/main/views.py
@@ -0,0 +1,10 @@
+from app import main
+from flask import render_template
+from app.requests import get_quotes
+from . import main
+
+@main.route('/')
+def index():
+    quotes = get_quotes()
+
+    return render_template('index.html', quotes = quotes)
--- app/requests.py
+++ app/requests.py
@@ -1,15 +1,10 @@
 import requests as rq
-# base_url = None
-
-# def configure_requests():
-#     global base_url
-#     base_url = app.config['BASE_URL']
 
 base_url='http://quotes.stormconsultancy.co.uk/random.json'
 
 def get_quotes():
 
     with rq.get(base_url) as data:
-       quotes = data.json
+       quotes = data.json()
 
     return quotes
