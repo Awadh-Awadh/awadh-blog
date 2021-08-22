d933ee950674db3617334036e3153f99fdd09f8e
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-22 10:54:51 +0300

    add db initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 
@@ -18,5 +21,9 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/auth')
+
+    #initializinga extensions
+
+    db.init_app(app)
 
     return app
--- manage.py
+++ manage.py
@@ -1,4 +1,10 @@
-from app import create_app
+from app import create_app, db
 
 
 app = create_app('development')
+
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app = app, db = db)
