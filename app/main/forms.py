f4f4e7053cacff03ee33caddb1a8de89adde90e0
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-23 15:35:33 +0300

    add logic for profile change for a user

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
 from  wtforms import SubmitField, StringField
+from wtforms.validators import ValidationError
+from ..models import User
 
 
 
@@ -7,3 +9,15 @@ class WriteForm(FlaskForm):
     title = StringField('Title')
     story = StringField('Write a story')
     submit = SubmitField('Publish')
+
+
+class EditProfile(FlaskForm):
+    username = StringField('Username')
+    email = StringField('Email')
+    submit = SubmitField('Update')
+
+
+    def username(self, field):
+        user = User.query.filter_by(username = field.data).all()
+        if user:
+            raise ValidationError('Username exists. Please choose another username')
