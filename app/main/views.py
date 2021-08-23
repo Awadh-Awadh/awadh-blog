0a8bc2527d9b71e64702b6284a29a8082c54d4ba
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-23 13:00:00 +0300

    add posts to the index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask_login import login_required
-from app import main
-from flask import render_template
+from app import main, db
+from flask import render_template,redirect,url_for
 from app.requests import get_quotes
 from . import main
 from .forms import WriteForm
+from ..models import User, Posts
 
 @main.route('/')
 def index():
@@ -16,5 +17,15 @@ def index():
 @login_required
 def write():
     form = WriteForm()
-    return render_template('write.html', form = form)
+    if form.validate_on_submit():
+        post = Posts(title = form.title.data, conent = form.story.data)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('main.account'))
+    posts = Posts.query.all()
+    return render_template('index.html', form = form, posts = posts)
 
+@main.route('/account')
+def account():
+
+    return render_template('account.html')
